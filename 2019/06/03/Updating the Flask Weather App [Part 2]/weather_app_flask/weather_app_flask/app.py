749453c472c246b7f0b4e860a5dedd0a8080b483
Anthony Herbert <anthony@anthonyherbert.com>
2019-06-03 08:27:27 -0700

    added weather app flask update video

---
+++ 2019/06/03/Updating the Flask Weather App [Part 2]/weather_app_flask/weather_app_flask/app.py
@@ -0,0 +1,80 @@
+import requests
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecret'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+def get_weather_data(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    r = requests.get(url).json()
+    return r
+
+@app.route('/')
+def index_get():
+    cities = City.query.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = get_weather_data(city.name)
+        print(r)
+
+        weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
+
+
+    return render_template('weather.html', weather_data=weather_data)
+
+@app.route('/', methods=['POST'])
+def index_post():
+    err_msg = ''
+    new_city = request.form.get('city')
+        
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+
+        if not existing_city:
+            new_city_data = get_weather_data(new_city)
+
+            if new_city_data['cod'] == 200:
+                new_city_obj = City(name=new_city)
+
+                db.session.add(new_city_obj)
+                db.session.commit()
+            else:
+                err_msg = 'City does not exist in the world!'
+        else:
+            err_msg = 'City already exists in the database!'
+
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City added succesfully!')
+
+    return redirect(url_for('index_get'))
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    city = City.query.filter_by(name=name).first()
+    db.session.delete(city)
+    db.session.commit()
+
+    flash(f'Successfully deleted { city.name }', 'success')
+    return redirect(url_for('index_get'))
