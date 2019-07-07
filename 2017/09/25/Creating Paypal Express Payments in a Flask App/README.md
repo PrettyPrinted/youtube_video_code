6a14b74fa453391004ed0b0e405ad9caa269397f
Anthony Herbert <anthony@anthonyherbert.com>
2019-07-07 07:49:01 -0700

    added paypal video

---
+++ 2017/09/25/Creating Paypal Express Payments in a Flask App/README.md
@@ -0,0 +1,1 @@
+Video: https://www.youtube.com/watch?v=1iFE_5wD2Q0
---
+++ 2017/09/25/Creating Paypal Express Payments in a Flask App/paypal/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, jsonify, request
+import paypalrestsdk
+
+app = Flask(__name__)
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "CLIENT_ID",
+  "client_secret": "CLIENT_SECRET" })
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "testitem",
+                    "sku": "12345",
+                    "price": "500.00",
+                    "currency": "USD",
+                    "quantity": 1}]},
+            "amount": {
+                "total": "500.00",
+                "currency": "USD"},
+            "description": "This is the payment transaction description."}]})
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID' : payment.id})
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id' : request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success' : success})
+
+if __name__ == '__main__':
+    app.run(debug=True)
