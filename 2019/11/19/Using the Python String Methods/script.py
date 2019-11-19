3df288de8a35e657af71c6e1fb68753ce498445b
Anthony Herbert <anthony@prettyprinted.com>
2019-11-19 05:07:28 -0800

    added string methods video

---
+++ 2019/11/19/Using the Python String Methods/script.py
@@ -0,0 +1,35 @@
+mystr = 'The Quick BROWN fox jumps over the lazy dog.'
+
+#print(mystr.capitalize())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.swapcase())
+#print(mystr.title())
+
+#print(mystr.count('O'))
+#print(mystr.endswith('dog'))
+#print(mystr.startswith('The'))
+#print(mystr.find('cat'))
+#print(mystr.rfind('he'))
+#print(mystr.index('cat'))
+
+#print('123AC'.isalnum())
+#print('abcABC'.isalpha())
+#print('abcABC'.isascii())
+#isdecimal, isdigit, islower, isnumeric, isspace, istitle, isupper
+
+#print(mystr.center(100))
+#print(mystr.ljust(100, '-'))
+#print(mystr.rjust(100, '-'))
+
+#mystr = '      The Quick BROWN fox jumps over the lazy dog.           '
+#print(mystr.strip())
+
+#print(mystr.partition('he'))
+#print(mystr.rpartition('he'))
+#print(mystr.split('he'))
+
+text = '''Anthony created Pretty Printed in
+the year 2015.'''
+
+print(text.splitlines())
