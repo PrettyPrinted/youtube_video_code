ae9266ddff7145c7e839495db72818534b40f697
Anthony Herbert <anthony@prettyprinted.com>
2022-01-05 23:28:01 +0700

    added many to many video

---
+++ 2022/01/05/Creating Many-To-Many Relationships in Flask-SQLAlchemy/many_to_many_example/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+user_channel = db.Table('user_channel',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('channel_id', db.Integer, db.ForeignKey('channel.id'))
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    following = db.relationship('Channel', secondary=user_channel, backref='followers')
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
+
+class Channel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<Channel: {self.name}>'
