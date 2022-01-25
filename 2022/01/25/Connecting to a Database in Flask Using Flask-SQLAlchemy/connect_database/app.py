80f813715089c1ac11d5bb4d484f2a7b6af5fff9
Anthony Herbert <anthony@prettyprinted.com>
2022-01-25 22:21:07 +0700

    added connecting to database video

---
+++ 2022/01/25/Connecting to a Database in Flask Using Flask-SQLAlchemy/connect_database/app.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True)
+    date_joined = db.Column(db.Date, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User: {self.email}>'
