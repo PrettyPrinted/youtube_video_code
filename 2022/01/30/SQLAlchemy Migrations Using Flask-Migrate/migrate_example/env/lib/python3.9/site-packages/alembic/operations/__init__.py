99a0c7c85b6976797233b24d45ba5e430f9a8b36
Anthony Herbert <anthony@prettyprinted.com>
2022-01-30 08:24:14 +0700

    added migrations video

--- 2022/01/30/SQLAlchemy Migrations Using Flask-Migrate/migrate_example/env/lib/python3.9/site-packages/alembic/operations/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import toimpl
-from .base import BatchOperations
-from .base import Operations
-from .ops import MigrateOperation
-
-
-__all__ = ["Operations", "BatchOperations", "MigrateOperation"]
