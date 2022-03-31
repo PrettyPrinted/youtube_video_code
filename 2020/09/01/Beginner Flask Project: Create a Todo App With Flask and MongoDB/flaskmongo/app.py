3d31506af400b0e7bfbded869cdfcecbaa5efa7b
Vodka-X <95764863+Vodka77@users.noreply.github.com>
2022-03-31 22:41:52 +0200

    Update app.py

--- 2020/09/01/Beginner Flask Project: Create a Todo App With Flask and MongoDB/flaskmongo/app.py
+++ 2020/09/01/Beginner Flask Project: Create a Todo App With Flask and MongoDB/flaskmongo/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+#hello
 app = Flask(__name__)
 
 app.config['MONGO_URI'] = 'mongodb+srv://admin:Password1@cluster0.jxvdd.mongodb.net/mydatabase?retryWrites=true&w=majority'
