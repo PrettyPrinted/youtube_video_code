4843da3b0285da6891c5e4e1d46fad756ce71ccd
Anthony Herbert <anthony@prettyprinted.com>
2022-10-14 15:36:41 +0700

    added flask socketio video

---
+++ 2016/06/27/Creating a Chat App With Flask-SocketIO/README.md
@@ -0,0 +1,1 @@
+Video: https://www.youtube.com/watch?v=RdSrkkrj3l4
---
+++ 2016/06/27/Creating a Chat App With Flask-SocketIO/flask-socketio-chat/main.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
+
+if __name__ == '__main__':
+	socketio.run(app)
