885e835d96e2257cd9ec5bf9eb9e002e6fd03274
Anthony Herbert <anthony@prettyprinted.com>
2022-10-14 15:32:20 +0700

    added weather api video

---
+++ 2016/05/20/Python Requests Tutorial - How to Call a Weather API/README.md
@@ -0,0 +1,1 @@
+Video: https://www.youtube.com/watch?v=sbYXa6HJJ5M
---
+++ 2016/05/20/Python Requests Tutorial - How to Call a Weather API/python-weather-api/test.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/temperature', methods=['POST'])
+def temperature():
+    zipcode = request.form['zip']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    json_object = r.json()
+    temp_k = float(json_object['main']['temp'])
+    temp_f = (temp_k - 273.15) * 1.8 + 32
+    return render_template('temperature.html', temp=temp_f)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
