5654e5feba854d3b41b8dd75218e0221408e7831
Anthony Herbert <anthony@prettyprinted.com>
2023-08-11 14:07:45 +0700

    added script to load data to code

--- 2023/08/08/How to Create an Instant Search Bar With Flask and HTMX/htmx_search_example/app.py
+++ 2023/08/08/How to Create an Instant Search Bar With Flask and HTMX/htmx_search_example/app.py
@@ -39,3 +39,30 @@ def create_app():
     app.register_blueprint(main)
 
     return app
+
+
+# You can run this function in the flask shell after creating the database
+def load_data():
+    # You can download the CSV from the following link.
+    # https://github.com/HipsterVizNinja/random-data/tree/main/Music/hot-100
+    song_data = {}
+    with open("data.csv") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if (row["song"], row["performer"]) not in song_data:
+                song_data[(row["song"], row["performer"])] = {
+                    "chart_debut": row["chart_debut"],
+                    "peak_position": int(row["peak_position"]),
+                    "time_on_chart": int(row["time_on_chart"])
+                }
+            else:
+                song_data[(row["song"], row["performer"])]["peak_position"] = min(song_data[(row["song"], row["performer"])]["peak_position"], int(row["peak_position"]))
+                song_data[(row["song"], row["performer"])]["time_on_chart"] = max(song_data[(row["song"], row["performer"])]["time_on_chart"], int(row["time_on_chart"]))
+
+    for s in song_data:
+        song = Song(title=s[0], performer=s[1], chart_debut=song_data[s]["chart_debut"], peak_position=song_data[s]["peak_position"], time_on_chart=song_data[s]["time_on_chart"])
+        db.session.add(song)
+
+
+    db.session.commit() 
+
